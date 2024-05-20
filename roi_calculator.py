e56585a4dd93496fe5721adff840c3cd302812b3
joaodias004 <jdrdl11@gmail.com>
2024-05-20 10:43:35 -0300

    Added error handling.
    Added error handling in case a non numeric value is inserted.

--- roi_calculator.py
+++ roi_calculator.py
@@ -7,13 +7,19 @@ def calculando_ROI():
     roi = (total_lucro - total_custos) / total_custos * 100
     return roi
 
-total_custos = float(input("Qual foi o custo total do empreendimento?" ))
-total_lucro = float(input("Qual foi o lucro total?" ))
+try:
+    total_custos = float(input("Qual foi o custo total do empreendimento?" ))
+    total_lucro = float(input("Qual foi o lucro total?" ))
+except:
+    print("Por favor, insira apenas valores númericos.")
+    exit()
 
 lucro = round(calculando_lucro(), 2)
 roi = round(calculando_ROI(), 2)
 
 calculando_lucro()
 calculando_ROI()
+
+
 print(f"O lucro total foi de: R${lucro}")
 print(f"o ROI de seu empreendimento foi: {roi}%")
